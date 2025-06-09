b2bb3524dc760fd1ef1094a3a55d53606c38400d
Josue Paz <josuepaz964@gmail.com>
2025-06-09 10:45:26 -1200

    DTO

--- src/fuel-types/dto/create-fuel-type.dto.ts
+++ src/fuel-types/dto/create-fuel-type.dto.ts
@@ -1,1 +1,8 @@
-export class CreateFuelTypeDto {}
+import { IsNotEmpty, IsString, MaxLength } from 'class-validator';
+
+export class CreateFuelTypeDto {
+  @IsString()
+  @IsNotEmpty({ message: 'El nombre del tipo de combustible es obligatorio' })
+  @MaxLength(100, { message: 'El nombre no debe tener más de 100 caracteres' })
+  name: string;
+}
